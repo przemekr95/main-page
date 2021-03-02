fb71e98ede6c5d95f0ae2eaabc38fdb0c74b82f7
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-03-02 22:13:44 +0100

    add hover on background

---
+++ src/components/Background/Background.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Background.css'
+
+const Background = () => {
+    return(
+    <>
+        <section className="wrapper-bg">
+            <div className="bg"></div>
+            <div className="bg"></div>
+        </section>
+    </>
+    )
+}
+
+export default Background
